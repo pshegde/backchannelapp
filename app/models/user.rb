665dc1f2e1fe05ee1a08d8e5f1454113d21ca167
Prajakta Hegde <pshegde@ncsu.edu>
2012-09-16 15:02:38 -0400

    user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
+require 'digest/sha1'
+
 class User < ActiveRecord::Base
   attr_accessible :password, :username
   validates_length_of :username, :within => 4..10
   validates_presence_of :password, :username
-
- # attr_accessor :password, :username
+  attr_protected :id, :salt
+  attr_accessor :password    #, :username
 
 
   def self.authenticate(username, password)
@@ -18,4 +20,21 @@ class User < ActiveRecord::Base
     nil
   end
 
+  def self.random_string(len)
+    #generate a random password consisting of strings and digits
+    chars = ("a".."z").to_a + ("A".."Z").to_a + ("0".."9").to_a
+    newpass = ""
+    1.upto(len) { |i| newpass << chars[rand(chars.size-1)] }
+    return newpass
+  end
+
+  def password=(pass)
+    @password=pass
+    self.salt = User.random_string(10) if !self.salt?
+    self.hashed_password = User.encrypt(@password, self.salt)
+  end
+
+  def self.encrypt(pass, salt)
+    Digest::SHA1.hexdigest(pass+salt)
+  end
 end
