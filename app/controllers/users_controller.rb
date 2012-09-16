bc5d7b6f1ad0c0a02d95d1e11001cc11c31146ca
Prajakta Hegde <pshegde@ncsu.edu>
2012-09-16 11:45:36 -0400

    final working model day 1. Added buttons on login page.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,24 +2,12 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   def index
-    flash[:message]  = "in login"
-    if request.post?
-      if session[:users] = User.authenticate(params[:users][:username], params[:users][:password])
-        puts "***authenticated!"
-        flash[:message]  = "Login successful"
-        @users = User.all
+    @users = User.all
 
-        respond_to do |format|
-          format.html # index.html.erb
-          format.json { render json: @users }
-        end
-        #redirect_to_stored
-      else
-        flash[:warning] = "Login unsuccessful"
-
-      end
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @users }
     end
-
   end
 
   # GET /users/1
@@ -52,15 +40,26 @@ class UsersController < ApplicationController
   # POST /users
   # POST /users.json
   def create
-    @user = User.new(params[:user])
+    flash[:message]  = "in login"
+    if request.post?
+      if session[:user] = User.authenticate(params[:user][:username], params[:user][:password])
+        puts "***authenticated!"
+        @user = User.new(params[:user])
 
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render json: @user, status: :created, location: @user }
+        respond_to do |format|
+          if @user.save
+            format.html { redirect_to @user, notice: 'User was successfully created.' }
+            format.json { render json: @user, status: :created, location: @user }
+          else
+            format.html { render action: "new" }
+            format.json { render json: @user.errors, status: :unprocessable_entity }
+          end
+        end
+        flash[:message]  = "Login successful"
+        #redirect_to_stored
       else
-        format.html { render action: "new" }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+        flash[:warning] = "Login unsuccessful"
+        redirect_to(:controller=>:users,:action=>:new)
       end
     end
   end
