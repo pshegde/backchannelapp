a40de282a40bd976862f985aa54095c6c2402a02
Prajakta Hegde <pshegde@ncsu.edu>
2012-09-16 18:36:39 -0400

    migrate db changes

---
+++ db/migrate/20120916195236_create_user.rb
@@ -0,0 +1,18 @@
+class CreateUser < ActiveRecord::Migration
+  def up
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :username
+      t.string :password
+      t.string :salt
+      t.boolean :isadmin
+      t.timestamps
+    end
+  end
+
+  def down
+    drop_table :users
+  end
+end
