9ff677353c7aacc769668d5e962b0766ad1c7658
suneetsasidharan <suneet.sasidharan@gmail.com>
2012-09-17 23:28:31 -0400

    Posts with category and username _ Suneet

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-  attr_accessible :title, :content
+  attr_accessible :title, :content , :Category_id
   belongs_to :category
   belongs_to :user
   #:order => "created_at"
