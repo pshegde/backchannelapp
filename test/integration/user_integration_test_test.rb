ed1278ffc432902b541fae64cde1d5f45d854e95
pshegde@ncsu.edu <pshegde@ncsu.edu>
2012-09-30 17:29:57 -0400

    integration ...almost everything except create comment

--- test/integration/user_integration_test_test.rb
+++ test/integration/user_integration_test_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class UserIntegrationTestTest < ActionDispatch::IntegrationTest
-  fixtures :users
+  fixtures :all
   test "the truth" do
    assert true
   end
@@ -14,12 +14,52 @@ class UserIntegrationTestTest < ActionDispatch::IntegrationTest
     assert_equal '/posts/index', path
     end
   end
-  def test_new_user
+  def test_flow
+    # create user
     get "/users/new"
     assert_response :success
     post_via_redirect "/users"   ,:user=>{:username => "suneet" , :first_name =>"suneet" ,:last_name=> "sasidharan", :email =>"ssasidh@ncsu.edu",:original_password =>"suneet" }
-    #click_button "Create User"
+
     @user = User.find_all_by_username("suneet").first
+    assert_equal 'User was successfully created.',flash[:notice]
     assert_equal '/users/'+ @user.id.to_s , path
+
+    #create post
+    get "posts/new"
+    assert_response :success
+    @category = Category.find_all_by_name("MyString").first
+    @request.session[:user_id] = @user
+    post_via_redirect "/posts" ,:post => {:content=>"test post",:Category_id=>@category.id , :title=>"test title", :num_votes=>1 }
+    @post = Post.find_all_by_content("test post").first
+    assert_equal 'Post was successfully created.',flash[:notice]
+    assert_equal '/posts/'+ @post.id.to_s ,path
+
+    # votes
+    @request.session[:user_id] = @user
+    #@request.session[:post_id_passed] = posts(:onepost).id
+    # vote on someone else's post
+    get_via_redirect '/voteforpost/incrementVote?post_id_for_vote='+posts(:onepost).id.to_s
+    assert_equal 'Your vote was registered successfully.' ,flash[:alert]
+
+    #vote on your own post
+    get_via_redirect '/voteforpost/incrementVote?post_id_for_vote='+@post.id.to_s
+    assert_equal 'Sorry. You cannot vote for your own post.' ,flash[:alert]
+
+    get_via_redirect '/commentsforpost/new?post_id_passed='+@post.id.to_s , :comment =>{:content=>"test comment"}
+    assert_equal 'Comment was successfully created.' ,flash[:alert]
+
+
+    #logout
+    get "logout"
+    assert_equal 'Successfully Logged out', flash[:alert]
+
+    if false
+    #post_via_redirect "/commentsforpost/new?post_id_passed="+@post.id.to_s , :comment => {:content=>"test comment" }
+    post_via_redirect "/comments", :comment => {:content=>"test comment" }
+    get "comment"
+    @comment = Comment.find_all_by_content("test comment").first
+    assert_equal 'Comment was successfully created.',flash[:notice]
+    #assert_equal '/comment/'+ @comment.id.to_s, path
+       end
   end
 end
