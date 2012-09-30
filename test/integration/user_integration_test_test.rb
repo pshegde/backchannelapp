2a78b59a622d38dd71fff75fa9e638a1d631d533
pshegde@ncsu.edu <pshegde@ncsu.edu>
2012-09-30 13:54:44 -0400

    integration ...v2

--- test/integration/user_integration_test_test.rb
+++ test/integration/user_integration_test_test.rb
@@ -5,15 +5,20 @@ class UserIntegrationTestTest < ActionDispatch::IntegrationTest
   test "the truth" do
    assert true
   end
-
+   if false
   def test_login_page
     https!
     get "logins/new"
     assert_response :success
-    post_via_redirect "logins/new"  , :username => users(:one).username, :password => "password"
-
-    #post_via_redirect "/logins", :username => users(:one).username, :password => "password"
+    post_via_redirect "/loginUser"  , :username => users(:one).username, :original_password => "password"
     assert_equal '/posts/index', path
-
+    end
+  end
+  def test_new_user
+    get "/users/new"
+    assert_response :success
+    post_via_redirect "/users/new"   ,:username => "suneet" , :first_name =>"suneet" ,:last_name=> "sasidharan", :email =>"ssasidh@ncsu.edu",:original_password =>"suneet"
+    #click_button "Create User"
+    assert_equal '/users/show' ,path
   end
 end
